6ecdca4e6bb8c073dc36e96a5d1bec7854e250f7
Unknown <arindamTech@outlook.com>
2017-06-24 21:42:07 +0800

    first commit - basic structure

---
+++ libs/scripts/core.js
@@ -0,0 +1,17 @@
+
+
+//init Angular Code block
+var app = angular.module('flickUIApp', []);
+app.controller('flickAppController', function ($scope) {
+    $scope.pagetitle = "Flickr UI Application";
+    
+});
+
+
+$(document).ready(function () {
+
+    
+
+
+
+});
