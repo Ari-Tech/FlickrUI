ae491c0621adf1c4048cbf65a2f4e516610913d0
Unknown <arindamTech@outlook.com>
2017-06-26 17:23:17 +0800

    Grid and Table layout
    Button toggle for layout change and render images with title

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
+<!-- 
 FlickrUI
+ -->
+
+
+<!-- 
+Install Node http server :
+ -->
+
+
+npm install http-server -g 
+
+<!-- 
+Once done,
+double click on server.bat and ther application will run on 127.0.0.1:8080
+
+Go to browser and type: "http://127.0.0.1:8080/" the UI should load
+
+On mac, Open terminal in Application path:
+ -->
+
+cd /ApplicationPath
+
+http-server
+
+<!-- 
+Go to browser and type: "http://127.0.0.1:8080/" the UI should load
+ -->
--- libs/scripts/core.js
+++ libs/scripts/core.js
@@ -4,7 +4,7 @@
 // }
 //init Angular Code block
 var app = angular.module('flickUIApp', []);
-app.controller('flickAppController', function ($scope, $http) {
+app.controller('flickAppController', function ($scope, $http, $compile) {
     $scope.pagetitle = "Flickr UI Application";
     $scope.displayType = "fa fa-th-large";
     $scope.Action = {
@@ -14,30 +14,73 @@ app.controller('flickAppController', function ($scope, $http) {
             } else {
                 $scope.displayType = "fa fa-th-large";
             }
+            document.getElementById('photoSection').innerHTML="";
+            $scope.Action.refreshLayout();
+        },
+        refreshLayout: function(){
+            $http.get("https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1")
+        .then(function (response) {
+            $scope.flickrItems = response.data.items;
+            $scope.count = response.data.items.length;
+            //$scope.text = response.data.items[0].title;
+            var iterator = 0;
+
+            if ($scope.displayType === 'fa fa-th-large') {
+                var k = 0;
+                var innerHTML = "";
+                var rows = Math.ceil($scope.count / 3);
+                var cols = 3;
+                for (var i = 0; i < rows; i++) {
+                    innerHTML += '<div class="row gridlayout"><div class="col-md-1"></div>';
+                    for (var j = 0; j < cols; j++) {
+
+                        if ($scope.flickrItems[k]) {
+                            $scope["data" + k] = $scope.flickrItems[k];
+                            var imageURL = $scope["data" + k].media.m;
+                            var text = $scope["data" + k].title;
+                            innerHTML += '<div class="col-md-3"><div class="thumbnail"><img src="' + $scope["data" + k].media.m + '"><div class="caption"><h6>' + $scope["data" + k].title + '</h6></div></div></div>';
+                            k++;
+                        }
+                    }
+                    innerHTML += "</div>";
+
+                }
+                var newdom = $compile(innerHTML)($scope);
+                angular.element(document.getElementById('photoSection')).append(newdom);
+
+
+            } else {
+                for (var i = 0; i < $scope.flickrItems.length; i++) {
+                    $scope["data" + i] = $scope.flickrItems[i];
+                    var innerHTML = "<photo-template info='" + "data" + i + "'></photo-template>";
+                    var newdom = $compile(innerHTML)($scope);
+                    angular.element(document.getElementById('photoSection')).append(newdom);
+
+                }
+            }
+
+            $scope.$apply();
+        });
         }
     };
-$http.get("https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1")
-    .then(function(response) {
-        //console.log(response.data)
-        $scope.flickrItems=response.data.items;
-        $scope.count=response.data.items.length;
-        $scope.rows=Math.ceil(response.data.items.length/3);
-        $scope.cols=3;
-        $scope.imageurl=response.data.items[0].media.m;
-        $scope.text=response.data.items[0].title;
-    });
+    $scope.Action.refreshLayout();
+    // $timeout(function () {
+    //     $scope.Action.refreshLayout();
+    // }, 200);
+    
 })
-.directive('photoTemplate', function() {
-  return {
-    templateUrl: 'photo-template.html'
-  };
-});
+    .directive('photoTemplate', function () {
+        return {
+            restrict: 'AE',
+            scope: {
+                flickInfo: '=info'
+            },
+            templateUrl: 'photo-template.html'
+        };
+    });
 
 
 $(document).ready(function () {
-
-
-
 
 
 });
