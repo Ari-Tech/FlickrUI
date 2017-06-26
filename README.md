85b269955a647939b3f86ca46ba9bbcddaa4f453
Unknown <arindamTech@outlook.com>
2017-06-27 00:39:20 +0800

    directory template for grid layout
    change grid layout to directory based approach

--- README.md
+++ README.md
@@ -1,28 +1,29 @@
-<!-- 
+
 FlickrUI
- -->
+---------
 
 
-<!-- 
+
+
 Install Node http server :
- -->
 
 
-npm install http-server -g 
 
-<!-- 
+`npm install http-server -g`
+
+
 Once done,
 double click on server.bat and ther application will run on 127.0.0.1:8080
 
 Go to browser and type: "http://127.0.0.1:8080/" the UI should load
 
 On mac, Open terminal in Application path:
- -->
 
-cd /ApplicationPath
 
-http-server
+`cd /ApplicationPath`
 
-<!-- 
-Go to browser and type: "http://127.0.0.1:8080/" the UI should load
- -->
+`http-server`
+
+
+Go to browser and type: `"http://127.0.0.1:8080/"` the UI should load
+
--- libs/scripts/core.js
+++ libs/scripts/core.js
@@ -1,7 +1,4 @@
-// var feed = '';
-// function jsonFlickrFeed(d) {
-//     feed = d;
-// }
+
 //init Angular Code block
 var app = angular.module('flickUIApp', []);
 app.controller('flickAppController', function ($scope, $http, $compile, $timeout) {
@@ -14,60 +11,60 @@ app.controller('flickAppController', function ($scope, $http, $compile, $timeout
             } else {
                 $scope.displayType = "fa fa-th-large";
             }
-            document.getElementById('photoSection').innerHTML="";
+            document.getElementById('photoSection').innerHTML = "";
             $scope.Action.refreshLayout();
         },
-        refreshLayout: function(){
+        refreshLayout: function () {
             $http.get("https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1")
-        .then(function (response) {
-            $scope.flickrItems = response.data.items;
-            $scope.count = response.data.items.length;
-            //$scope.text = response.data.items[0].title;
-            var iterator = 0;
+                .then(function (response) {
+                    $scope.flickrItems = response.data.items;
+                    $scope.count = response.data.items.length;
+                    //$scope.text = response.data.items[0].title;
+                    var iterator = 0;
 
-            if ($scope.displayType === 'fa fa-th-large') {
-                var k = 0;
-                var innerHTML = "";
-                var rows = Math.ceil($scope.count / 3);
-                var cols = 3;
-                for (var i = 0; i < rows; i++) {
-                    innerHTML += '<div class="row gridlayout"><div class="col-md-1"></div>';
-                    for (var j = 0; j < cols; j++) {
+                    if ($scope.displayType === 'fa fa-th-large') {
+                        //grid View
+                        var k = 0;
+                        var innerHTML = "";
+                        var rows = Math.ceil($scope.count / 3);
+                        var cols = 3;
+                        for (var i = 0; i < rows; i++) {
+                            innerHTML += '<div class="row gridlayout"><div class="col-md-1"></div>';
+                            for (var j = 0; j < cols; j++) {
 
-                        if ($scope.flickrItems[k]) {
-                            $scope["data" + k] = $scope.flickrItems[k];
-                            var imageURL = $scope["data" + k].media.m;
-                            var text = $scope["data" + k].title;
-                            innerHTML += '<div class="col-md-3"><div class="thumbnail"><img src="' + $scope["data" + k].media.m + '"><div class="caption"><h6>' + $scope["data" + k].title + '</h6></div></div></div>';
-                            k++;
+                                if ($scope.flickrItems[k]) {
+                                    $scope["data" + k] = $scope.flickrItems[k];
+                                    innerHTML += "<gridphoto-template datainfo='" + "data" + k + "'></gridphoto-template>";
+                                    k++;
+                                }
+                            }
+                            innerHTML += "</div>";
+
+                        }
+                        var newdom = $compile(innerHTML)($scope);
+                        angular.element(document.getElementById('photoSection')).append(newdom);
+
+
+                    } else {
+                        //list view
+                        for (var i = 0; i < $scope.flickrItems.length; i++) {
+                            $scope["data" + i] = $scope.flickrItems[i];
+                            var innerHTML = "<photo-template info='" + "data" + i + "'></photo-template>";
+                            var newdom = $compile(innerHTML)($scope);
+                            angular.element(document.getElementById('photoSection')).append(newdom);
+
                         }
                     }
-                    innerHTML += "</div>";
 
-                }
-                var newdom = $compile(innerHTML)($scope);
-                angular.element(document.getElementById('photoSection')).append(newdom);
-
-
-            } else {
-                for (var i = 0; i < $scope.flickrItems.length; i++) {
-                    $scope["data" + i] = $scope.flickrItems[i];
-                    var innerHTML = "<photo-template info='" + "data" + i + "'></photo-template>";
-                    var newdom = $compile(innerHTML)($scope);
-                    angular.element(document.getElementById('photoSection')).append(newdom);
-
-                }
-            }
-
-            $scope.$apply();
-        });
+                    $scope.$apply();
+                });
         }
     };
-   // $scope.Action.refreshLayout();
+    // $scope.Action.refreshLayout();
     $timeout(function () {
         $scope.Action.refreshLayout();
     }, 200);
-    
+
 })
     .directive('photoTemplate', function () {
         return {
@@ -77,10 +74,12 @@ app.controller('flickAppController', function ($scope, $http, $compile, $timeout
             },
             templateUrl: 'photo-template.html'
         };
+    }).directive('gridphotoTemplate', function () {
+        return {
+            restrict: 'AE',
+            scope: {
+                gridrowInfo: '=datainfo'
+            },
+            templateUrl: 'gridphoto-template.html'
+        };
     });
-
-
-$(document).ready(function () {
-
-
-});
